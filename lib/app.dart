dd67d95be0c66702618b48648ccddc3a62050f03
seftoner <seftoner@gmail.com>
2025-08-13 09:55:53 +0300

    chore: stage intermediate app/auth changes before rebase

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hommie/features/auth/application/app_state_handler.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hommie/core/bootstrap/app_startup.dart';
+import 'package:hommie/features/auth/infrastructure/providers/credential_repository_provider.dart';
+import 'package:hommie/features/auth/infrastructure/repositories/secure_credentials_storage.dart';
+import 'package:hommie/features/auth/presentation/screens/server_discovery_page.dart';
+import 'package:hommie/features/server_manager/application/server_manager.dart';
+import 'package:hommie/features/server_manager/domain/models/server_config.dart';
 import 'package:hommie/router/router.dart';
 import 'package:hommie/services/networking/connection_state_provider.dart';
 import 'package:hommie/ui/screens/widgets/offline_container.dart';
@@ -46,5 +53,65 @@ class _ServiceInitializer extends ConsumerWidget {
     ref.watch(appStateHandlerProvider);
     ref.watch(connectionStateProvider);
     return child;
+  }
+}
+
+class HommieAppV2 extends ConsumerWidget {
+  const HommieAppV2({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final activeServer = ref.watch(serverManagerProvider);
+
+    return activeServer.when(
+      data: (server) {
+        if (server == null) {
+          return const ServerDiscoveryPage();
+        }
+        final crendetialStorage =
+            SecureCredentialRepository(const FlutterSecureStorage(), server.id);
+
+        return ProviderScope(
+          overrides: [
+            credentialsRepositoryProvider.overrideWithValue(crendetialStorage),
+          ],
+          child: const _RootAppWidget(),
+        );
+      },
+      loading: () => MaterialApp(home: const AppStartup()),
+      error: (error, stack) => MaterialApp(home: ErrorWidget('Error: $error')),
+    );
+
+/*     return ProviderScope(
+      child: Consumer(builder: (context, ref, _) {
+        final activeServer = ref.watch(serverManagerProvider);
+        // return SecureCredentialRepository(const FlutterSecureStorage(), serverId);
+
+        return activeServer.when(
+          data: (server) {
+            if (server == null) {
+              return const ServerDiscoveryPage();
+            }
+
+            // ref.watch(credentialsRepositoryProvider);
+
+            return MaterialApp.router(
+              title: 'Hommie',
+              debugShowCheckedModeBanner: false,
+              showPerformanceOverlay: false,
+              routerConfig: ref.read(goRouterProvider),
+              theme: HommieMaterialTheme.platform().light(),
+              darkTheme: HommieMaterialTheme.platformDark().dark(),
+              supportedLocales: const [
+                Locale('en', ''),
+              ],
+              builder: (context, child) => OfflineContainer(child: child),
+            );
+          },
+          loading: () => const AppStartup(),
+          error: (error, stack) => Text('Error: $error'),
+        );
+      }),
+    ); */
   }
 }
