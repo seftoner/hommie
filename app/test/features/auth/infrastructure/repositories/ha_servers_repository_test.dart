160ed7dedeee88e58ae831090f10d336c9a52a6e
Roman.Tolstokorov <seftoner@gmail.com>
2025-12-01 11:09:13 +0200

    Remove unused CachedHAServersRepository from HAServersRepository tests

--- app/test/features/auth/infrastructure/repositories/ha_servers_repository_test.dart
+++ app/test/features/auth/infrastructure/repositories/ha_servers_repository_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hommie/core/infrastructure/logging/logger.dart';
 import 'package:hommie/features/auth/infrastructure/repositories/ha_servers_repository.dart';
-import 'package:hommie/features/auth/infrastructure/repositories/cached_ha_servers_repository.dart';
 
 import '../../../../utils/tests_logger.dart';
 
@@ -9,14 +8,9 @@ void main() {
   logger = testLogger;
   group('HAServersRepository', () {
     late HAServersRepository baseRepository;
-    late CachedHAServersRepository cachedRepository;
 
     setUp(() {
       baseRepository = HAServersRepository();
-      cachedRepository = CachedHAServersRepository(
-        baseRepository: baseRepository,
-        cacheDuration: const Duration(seconds: 5), // Short duration for testing
-      );
     });
 
     test('base repository should work without caching', () async {
